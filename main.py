ef874ff734ac405dd8134239736bde91354b5121
ajgames1006 <134740638+ajgames1006@users.noreply.github.com>
2024-11-08 12:33:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ import customtkinter
 clicks = 0
 clickup = 1
 cost = 15
-
+#updates the text on the button so we can always see the cost
 def updatebutton():
     button2.configure(text=f"upgrade (cost: {cost})",command=clickupgrade)
-
+#updates the label so we can always see our click lvl and our clicks
 def updatelable():
     lable1.configure(text=str(clicks))
     lable2.configure(text=f"click lvl {clickup}")
 
+#when we click the click button it gives us 1 click times how many upgrades we have then runs update lable so we can see out new click amount
 def click():
     global clicks, clickup
     clicks = 1 * clickup + clicks
     updatelable()
-
+#when we click the upgrade button so that we can upgrade how many clicks we get each click then it runs update lable.
 def clickupgrade():
     global clicks, clickup, cost
-
+#if clicks >= cost checks if we have the same or more clicks as how much it costs to upgrade.
     if clicks >= cost:
         clickup = clickup + 1
         clicks = clicks - cost
@@ -39,7 +40,7 @@ root = customtkinter.CTk()
 #geometry makes the size of our window or "root"
 root.geometry("500x450")
 
-#this makes the frame that we put all of our stuff in master must = root
+#this makes the frame that we put all of our stuff in, master must = root
 frame = customtkinter.CTkFrame(master=root)
 #element.pack is needed for all elements or they will not show
 frame.pack(pady=10, padx=12)
